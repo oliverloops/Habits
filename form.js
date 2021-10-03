edc497537150e0de9c590b3ed8190403defd902c
oliverloops <oliveralr@protonmail.com>
2021-10-03 15:48:31 -0500

    Form added

--- form.js
+++ form.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 const Form = () => {
   return (
-    <View>
-      <Text>This is Form View</Text>
-    </View>
+    <>
+      <View style={styles.main}>
+        <Text style={{ fontSize: 14, color: "gray" }}>TITULO</Text>
+        <View style={{ marginTop: 15 }}>
+          <TextInput style={styles.input} />
+        </View>
+        <Text style={{ marginTop: 20, fontSize: 14, color: "gray" }}>
+          DESCRIPCIÓN
+        </Text>
+        <View style={{ marginTop: 15 }}>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+      <View style={{ flex: 3 }}>
+        <Button title="Guardar" />
+      </View>
+    </>
   );
 };
 
 export default Form;
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    marginTop: "8%",
+    marginLeft: "5%",
+    marginRight: "5%",
+  },
+  input: {
+    height: 38,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    padding: 10,
+  },
+});
