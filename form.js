e4313158d3226c7a92992105c4fa66193e682752
oliverloops <oliveralr@protonmail.com>
2021-10-03 19:21:39 -0500

    Latest bugs fixed

--- form.js
+++ form.js
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+//Context
+import { habitContext } from "./App";
 
-const Form = () => {
-  const [state, setState] = useState("");
+const Form = ({ navigation }) => {
+  const consumer = useContext(habitContext);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
 
-  const updateText = (value) => {
-    setState(value);
+  const updateTitle = (value) => {
+    setTitle(value);
+  };
+
+  const updateDesc = (value) => {
+    setDesc(value);
+  };
+
+  const storeHabit = () => {
+    let data = {
+      title: title,
+      desc: desc,
+      days: 0,
+      icon: "💻",
+      color: "hsl(36, 95%, 76%)",
+    };
+    consumer.push(data);
+    setTitle("");
+    setDesc("");
+    navigation.navigate("Home");
   };
 
   return (
     <>
       <View style={styles.main}>
-        <Text>{state}</Text>
         <Text style={{ fontSize: 14, color: "gray" }}>TITULO</Text>
         <View style={{ marginTop: 15 }}>
           <TextInput
             onChangeText={(text) => {
-              updateText(text);
+              updateTitle(text);
             }}
             style={styles.input}
           />
@@ -25,11 +46,16 @@ const Form = () => {
           DESCRIPCIÓN
         </Text>
         <View style={{ marginTop: 15 }}>
-          <TextInput style={styles.input} />
+          <TextInput
+            onChangeText={(text) => {
+              updateDesc(text);
+            }}
+            style={styles.input}
+          />
         </View>
       </View>
       <View style={{ flex: 3 }}>
-        <Button title="Guardar" />
+        <Button title="Guardar" onPress={storeHabit} />
       </View>
     </>
   );
